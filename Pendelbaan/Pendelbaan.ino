7c7c7d60f65e3497310252e0ce9671511d9c821e
Rob Antonisse <robantonisse@gmail.com>
2019-02-10 11:12:08 +0100

    Pendel start

--- Pendelbaan/Pendelbaan.ino
+++ Pendelbaan/Pendelbaan.ino
@@ -1,15 +1,16 @@
 /*
  Name:		Pendelbaan.ino
  Created:	2/10/2019 11:11:06 AM
- Author:	gebruiker
+ Author:	Rob Antonisse
+ Web: www.wisselmotor.nl
 */
 
-// the setup function runs once when you press reset or power the board
+
 void setup() {
 
 }
 
-// the loop function runs over and over again until power down or reset
+
 void loop() {
   
 }
